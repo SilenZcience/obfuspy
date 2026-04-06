b5436abd32b407482e10bdc34dee997ffce40058
Silas Kraume <silas.kraume1552@gmail.com>
2026-04-06 21:20:42 +0200

    fix assignements with object subscript

--- obfuspy/layers/layer_d.py
+++ obfuspy/layers/layer_d.py
@@ -5,37 +5,52 @@ class Layer_D(ast.NodeTransformer):
     """
     Layer D obfuscates assignments in the AST.
     """
-    def __init__(self, _) -> None:
+    def __init__(self, _, __) -> None:
         pass
+
+    def _to_load_expr(self, target):
+        if isinstance(target, ast.Name):
+            return ast.Name(id=target.id, ctx=ast.Load())
+        if isinstance(target, ast.Attribute):
+            return ast.Attribute(
+                value=self._to_load_expr(target.value),
+                attr=target.attr,
+                ctx=ast.Load()
+            )
+        if isinstance(target, ast.Subscript):
+            return ast.Subscript(
+                value=self._to_load_expr(target.value),
+                slice=target.slice,
+                ctx=ast.Load()
+            )
+        return target
 
     def visit_Assign(self, node):
         # x = y = 2 -> y = 2; x = y
         if len(node.targets) > 1:
             result = []
+            current_value = node.value
             for target in reversed(node.targets):
-                if isinstance(target, ast.Name):
-                    assign = ast.Assign(
-                        targets=[target],
-                        value=node.value if target is node.targets[-1]
-                            else ast.Name(id=node.targets[node.targets.index(target) + 1].id,
-                                        ctx=ast.Load()),
-                        lineno=node.lineno,
-                        col_offset=node.col_offset
-                    )
-                    result.append(self.visit(assign))
+                assign = ast.Assign(
+                    targets=[target],
+                    value=current_value,
+                    lineno=node.lineno,
+                    col_offset=node.col_offset
+                )
+                result.append(self.visit(assign))
+                current_value = self._to_load_expr(target)
             return result
         # a, b = 1, 2 -> a = 1; b = 2
         if len(node.targets) == 1 and isinstance(node.targets[0], ast.Tuple):
             if isinstance(node.value, ast.Tuple):
                 result = []
                 for target, value in zip(node.targets[0].elts, node.value.elts):
-                    if isinstance(target, ast.Name):
-                        result.append(ast.Assign(
-                            targets=[target],
-                            value=value,
-                            lineno=node.lineno,
-                            col_offset=node.col_offset
-                        ))
+                    result.append(ast.Assign(
+                        targets=[target],
+                        value=value,
+                        lineno=node.lineno,
+                        col_offset=node.col_offset
+                    ))
                 return result
 
         self.generic_visit(node)
@@ -44,11 +59,11 @@ class Layer_D(ast.NodeTransformer):
     def visit_AugAssign(self, node):
         # x += 1 -> x = x + 1
         self.generic_visit(node)
-        if isinstance(node.target, ast.Name):
+        if isinstance(node.target, (ast.Name, ast.Attribute, ast.Subscript)):
             return ast.Assign(
                 targets=[node.target],
                 value=ast.BinOp(
-                    left=ast.Name(id=node.target.id, ctx=ast.Load()),
+                    left=self._to_load_expr(node.target),
                     op=node.op,
                     right=node.value
                 ),
