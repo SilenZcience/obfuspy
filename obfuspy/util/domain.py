2b0dfc71bc0cfb892e0cd296b74d23e90dba7930
Silas Kraume <silas.kraume1552@gmail.com>
2026-04-06 21:14:35 +0200

    updated domain

--- obfuspy/util/domain.py
+++ obfuspy/util/domain.py
@@ -1,16 +1,21 @@
 
 
 class File_Module:
-    def __init__(self, in_path: str, out_path: str) -> None:
+    def __init__(self, in_path: str, out_path: str, module_name: str = None) -> None:
         self.in_path = in_path
         self.out_path = out_path
+        self.module_name = module_name
         with open(in_path, 'r', encoding='utf-8') as f:
             self.in_code = f.read()
         self.out_code = ''
+        self.symtable = None
         self.tree = None
 
     def set_code(self, code: str) -> None:
         self.out_code = code
+
+    def set_symtable(self, symtable) -> None:
+        self.symtable = symtable
 
     def set_tree(self, tree: str) -> None:
         self.tree = tree
