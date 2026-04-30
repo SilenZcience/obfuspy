f3ddf921c9edb19c304098f5d1631d4cc072df46
Silas Kraume <silas.kraume1552@gmail.com>
2026-05-01 00:45:55 +0200

    refactored obfDefArguments

--- obfuspy/layers/obfDefArguments.py
+++ obfuspy/layers/obfDefArguments.py
@@ -1,87 +1,126 @@
 import ast
+import random
+
+from obfuspy.util.domain import SYMBOL_MAP, Node
+from obfuspy.util.randomizer import Randomizer
 
 
 class ObfDefArguments(ast.NodeTransformer):
     """
-    Obfuscates function/method arguments using the export map.
-    Only arguments present in the export map are renamed.
+    Obfuscates function/method arguments.
     """
-    def __init__(self, randomizer, file_module):
-        self.project_context = getattr(randomizer, 'project_context', {})
+    def __init__(self, randomizer: Randomizer, file_module):
+        self.randomizer = randomizer
         self.module_name = getattr(file_module, 'module_name', None)
-        self.arg_stack = [{}]
-        self.class_stack = []
-        self.function_stack = []
+        self.prefix_parts = []
+        self._defarg_name_map_stack = []  # Stack of dicts: {original_arg_name: obf_value}
+        self._defarg_name_map_cache = {}
 
-    def _arg_map_for_qualified(self, qualified_name):
-        symbol_map = self.project_context.get('symbol_map', {})
-        if not isinstance(symbol_map, dict):
-            return {}
-        prefix = f'{qualified_name}::arg::'
-        arg_map = {}
-        for key, value in symbol_map.items():
-            if not key.startswith(prefix):
-                continue
-            if isinstance(value, dict) and value.get('kind') != 'arg':
-                continue
-            original_arg = key[len(prefix):]
-            arg_map[original_arg] = value.get('name') if isinstance(value, dict) else value
-        return arg_map
+    def _push_defarg_name_scope(self):
+        key = tuple((lbl.ltype, lbl.name) for lbl in self.prefix_parts)
+        if key in self._defarg_name_map_cache:
+            defarg_name_map = self._defarg_name_map_cache[key]
+        else:
+            defarg_name_map = SYMBOL_MAP.get_defargs(self.prefix_parts)
+            self._defarg_name_map_cache[key] = defarg_name_map
+        self._defarg_name_map_stack.append(defarg_name_map)
 
-    def _current_callable_qualified_name(self, node):
-        parts = [self.module_name] if self.module_name else []
-        if self.class_stack:
-            parts.extend(self.class_stack)
-        if self.function_stack:
-            parts.extend(self.function_stack)
-        if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef)):
-            parts.append(node.name)
-        return '.'.join(part for part in parts if part)
+    def _pop_defarg_name_scope(self):
+        if self._defarg_name_map_stack:
+            self._defarg_name_map_stack.pop()
+
+    def _current_defarg_name_map(self):
+        return self._defarg_name_map_stack[-1] if self._defarg_name_map_stack else {}
 
     def _visit_callable(self, node):
-        qualified_name = self._current_callable_qualified_name(node)
-        arg_map = self._arg_map_for_qualified(qualified_name)
-        # Rename only arguments present in the export map
+        if isinstance(node, ast.Lambda):
+            defarg_name_map = {}
+            for arg in getattr(node.args, 'posonlyargs', []):
+                if arg:
+                    defarg_name_map[arg.arg] = {
+                        'name': next(self.randomizer.random_name_gen),
+                    }
+            for arg in getattr(node.args, 'args', []):
+                if arg:
+                    defarg_name_map[arg.arg] = {
+                        'name': next(self.randomizer.random_name_gen),
+                    }
+            for arg in getattr(node.args, 'kwonlyargs', []):
+                if arg:
+                    defarg_name_map[arg.arg] = {
+                        'name': next(self.randomizer.random_name_gen),
+                    }
+            if getattr(node.args, 'vararg', None):
+                defarg_name_map[node.args.vararg.arg] = {
+                    'name': next(self.randomizer.random_name_gen),
+                }
+            if getattr(node.args, 'kwarg', None):
+                defarg_name_map[node.args.kwarg.arg] = {
+                    'name': next(self.randomizer.random_name_gen),
+                }
+            self._defarg_name_map_stack.append(defarg_name_map)
+        else:
+            self.prefix_parts.append(Node.Def(node.name))
+            self._push_defarg_name_scope()
+            defarg_name_map = self._current_defarg_name_map()
+
         for arg in getattr(node.args, 'posonlyargs', []):
-            if arg and arg.arg in arg_map:
-                arg.arg = arg_map[arg.arg]
+            if arg and arg.arg in defarg_name_map:
+                arg.arg = defarg_name_map[arg.arg]['name']
         for arg in getattr(node.args, 'args', []):
-            if arg and arg.arg in arg_map:
-                arg.arg = arg_map[arg.arg]
+            if arg and arg.arg in defarg_name_map:
+                arg.arg = defarg_name_map[arg.arg]['name']
         for arg in getattr(node.args, 'kwonlyargs', []):
-            if arg and arg.arg in arg_map:
-                arg.arg = arg_map[arg.arg]
-        if getattr(node.args, 'vararg', None) and node.args.vararg.arg in arg_map:
-            node.args.vararg.arg = arg_map[node.args.vararg.arg]
-        if getattr(node.args, 'kwarg', None) and node.args.kwarg.arg in arg_map:
-            node.args.kwarg.arg = arg_map[node.args.kwarg.arg]
-        self.arg_stack.append(arg_map)
-        # Push function name for nested functions before visiting children
-        if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef)):
-            self.function_stack.append(node.name)
+            if arg and arg.arg in defarg_name_map:
+                arg.arg = defarg_name_map[arg.arg]['name']
+        if getattr(node.args, 'vararg', None) and node.args.vararg.arg in defarg_name_map:
+            node.args.vararg.arg = defarg_name_map[node.args.vararg.arg]['name']
+        if getattr(node.args, 'kwarg', None) and node.args.kwarg.arg in defarg_name_map:
+            node.args.kwarg.arg = defarg_name_map[node.args.kwarg.arg]['name']
+
+        r_vars = [next(SYMBOL_MAP.randomizer.random_name_gen) for _ in range(random.randint(1, 5))]
+
+        if not getattr(node.args, 'kwarg', None) and getattr(node.args, 'kwonlyargs', []):
+            for r_var in r_vars:
+                node.args.kwonlyargs.append(ast.arg(arg=r_var, annotation=None))
+                node.args.kw_defaults.append(ast.Constant(value=random.choice([None, True, False])))
+        elif not getattr(node.args, 'kwarg', None) and not getattr(node.args, 'vararg', None):
+            for r_var in r_vars:
+                node.args.args.append(ast.arg(arg=r_var, annotation=None))
+                node.args.defaults.append(ast.Constant(value=random.choice([None, True, False])))
+        else:
+            r_vars.clear()
 
         if isinstance(node, ast.Lambda):
             self.visit(node.body)
+            self._pop_defarg_name_scope()
         else:
             for stmt in node.body:
                 self.visit(stmt)
-        # Pop function name
-        if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef)):
-            self.function_stack.pop()
-        self.arg_stack.pop()
+            for r_var in r_vars:
+                r_pos = random.randint(0, len(node.body)-1)
+                node.body[r_pos:r_pos] = [ast.Assign(
+                    targets=[ast.Name(id=r_var, ctx=ast.Store())],
+                    value=ast.Name(id=r_var, ctx=ast.Load()),
+                    lineno=getattr(node.body[r_pos], 'lineno', 0),
+                    col_offset=getattr(node.body[r_pos], 'col_offset', 0),
+                )]
+
+            self._pop_defarg_name_scope()
+            self.prefix_parts.pop()
+
         return node
 
     def visit_Module(self, node):
-        self.arg_stack = [{}]
-        self.class_stack = []
-        self.function_stack = []
+        self.prefix_parts = [Node.Module(self.module_name)]
+        self._defarg_name_map_stack = []
         self.generic_visit(node)
         return node
 
     def visit_ClassDef(self, node):
-        self.class_stack.append(node.name)
+        self.prefix_parts.append(Node.Cls(node.name))
         self.generic_visit(node)
-        self.class_stack.pop()
+        self.prefix_parts.pop()
         return node
 
     def visit_FunctionDef(self, node):
@@ -94,8 +133,8 @@ class ObfDefArguments(ast.NodeTransformer):
         return self._visit_callable(node)
 
     def visit_Name(self, node):
-        for scope in reversed(self.arg_stack):
+        for scope in reversed(self._defarg_name_map_stack):
             if node.id in scope:
-                node.id = scope[node.id]
+                node.id = scope[node.id]['name']
                 break
         return node
