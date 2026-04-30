085f59d64ccea4736de266a57a7c836b8db104e7
Silas Kraume <silas.kraume1552@gmail.com>
2026-05-01 00:46:26 +0200

    refactored obfImports

--- obfuspy/layers/obfImports.py
+++ obfuspy/layers/obfImports.py
@@ -1,294 +1,131 @@
 import ast
+
+from obfuspy.util.domain import SYMBOL_MAP
 from obfuspy.util.randomizer import Randomizer
 
 
-class ObfImports(ast.NodeTransformer):
+class ObfImports(ast.NodeTransformer): # TODO: add symboltable verification to ensure we only obfuscate actual imports and not local variables with same name
     """
-    Obfuscates import-statements.
+    Obfuscates import statements and corresponding usages.
     """
-    global_import_map = {}  # Class variable: Maps module names to their global obfuscated aliases
-
     def __init__(self, randomizer: Randomizer, file_module) -> None:
         self.randomizer = randomizer
         self.file_module = file_module
-        self.project_context = getattr(randomizer, 'project_context', {})
-        self.import_map = {}
-        self.binding_map = {}
-        self.explicit_global_stack = []
-        self.scope_name_stack = []
+        self.module_name = getattr(file_module, 'module_name', None)
+        self._import_alias_stack = []  # Stack of dicts: {local_name: obf_name}
 
-    def _resolve_import_module(self, node) -> str:
-        module_name = getattr(self.file_module, 'module_name', None)
-        node_module = getattr(node, 'module', None)
-        if module_name is None:
-            return node_module
+    def _push_import_scope(self):
+        self._import_alias_stack.append({})
 
-        current_parts = module_name.split('.')
-        base_parts = current_parts[:-1]
-        level = getattr(node, 'level', 0)
+    def _pop_import_scope(self):
+        if self._import_alias_stack:
+            self._import_alias_stack.pop()
 
-        if level > 1:
-            base_parts = base_parts[:max(0, len(base_parts) - (level - 1))]
-
-        module_parts = list(base_parts)
-        if node_module:
-            module_parts.extend(node_module.split('.'))
-        return '.'.join(part for part in module_parts if part)
-
-    def _export_name(self, qualified_name: str):
-        entry = self.project_context.get('symbol_map', {}).get(qualified_name)
-        if isinstance(entry, dict):
-            return entry.get('name')
-        return entry
-
-    def _qualified_name(self, *parts) -> str:
-        return '.'.join(part for part in parts if part)
-
-    def _scope_key(self) -> tuple:
-        return tuple(self.scope_name_stack)
-
-    def _register_import(self, scope_key: tuple, original_name: str, local_name: str) -> None:
-        self.import_map.setdefault(scope_key, {})[original_name] = local_name
-
-    def _lookup_import(self, original_name: str):
-        for end_index in range(len(self.scope_name_stack), -1, -1):
-            scope_key = tuple(self.scope_name_stack[:end_index])
-            scope_map = self.import_map.get(scope_key)
-            if scope_map is not None and original_name in scope_map:
-                return scope_map[original_name]
-        return None
-
-    def _local_import_name(self, preferred_name: str, module_key: str = None) -> str:
-        """
-        Generate or retrieve a local import alias name.
-
-        If module_key is provided and already exists in the global import map,
-        reuse that alias. Otherwise, generate a new unique name and store it
-        in the global map if module_key is provided.
-        """
-        # If we have a module key and it's already been aliased, reuse that alias
-        if module_key and module_key in ObfImports.global_import_map:
-            return ObfImports.global_import_map[module_key]
-
-        # Generate a new unique name
-        generated_name = next(self.randomizer.random_name_gen)
-        while generated_name == preferred_name:
-            generated_name = next(self.randomizer.random_name_gen)
-
-        # Store in global map if module_key is provided
-        if module_key:
-            ObfImports.global_import_map[module_key] = generated_name
-
-        return generated_name
-
-    def _collect_import_bindings(self, node) -> None:
-        if isinstance(node, ast.ClassDef):
-            self.scope_name_stack.append(node.name)
-            for child in node.body:
-                self._collect_import_bindings(child)
-            self.scope_name_stack.pop()
-            return
-
-        if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef)):
-            self.scope_name_stack.append(node.name)
-            for child in node.body:
-                self._collect_import_bindings(child)
-            self.scope_name_stack.pop()
-            return
-
-        if isinstance(node, ast.Import):
-            scope_key = self._scope_key()
-            for name in node.names:
-                original_bound_name = name.asname or name.name.split('.')[0]
-                if name.asname is None and '.' in name.name:
-                    continue
-                # Use the full module name as the key for consistent aliasing
-                module_key = self._qualified_name(self._resolve_import_module(node), name.name)
-                local_name = self._local_import_name(name.name.split('.')[-1], module_key=module_key)
-                self._register_import(scope_key, original_bound_name, local_name)
-                self.binding_map[local_name] = module_key
-            return
-
-        if isinstance(node, ast.ImportFrom):
-            scope_key = self._scope_key()
-            resolved_module = self._resolve_import_module(node)
-            for name in node.names:
-                if name.name == '*':
-                    continue
-                original_bound_name = name.asname or name.name
-                qualified_name = f'{resolved_module}.{name.name}' if resolved_module else name.name
-                export_name = self._export_name(qualified_name)
-                imported_symbol_name = export_name if export_name is not None else name.name
-                # Use the qualified name as the key for consistent aliasing
-                local_name = self._local_import_name(imported_symbol_name, module_key=qualified_name)
-                self._register_import(scope_key, original_bound_name, local_name)
-                self.binding_map[local_name] = qualified_name
-            return
-
-        for field_name in ('body', 'orelse', 'finalbody'):
-            child_body = getattr(node, field_name, None)
-            if child_body:
-                for child in child_body:
-                    self._collect_import_bindings(child)
-        for handler in getattr(node, 'handlers', []):
-            for child in handler.body:
-                self._collect_import_bindings(child)
+    def _current_import_aliases(self):
+        return self._import_alias_stack[-1] if self._import_alias_stack else {}
 
     def visit_Module(self, node):
-        ObfImports.global_import_map = {}
-        self.import_map = {}
-        self.binding_map = {}
-        self.explicit_global_stack = []
-        self.scope_name_stack = []
-        self._collect_import_bindings(node)
+        self._import_alias_stack = []
+        self._push_import_scope()
+        self._collect_import_aliases(node)
         self.generic_visit(node)
+        self._pop_import_scope()
         return node
 
     def visit_ClassDef(self, node):
-        self.scope_name_stack.append(node.name)
+        self._push_import_scope()
+        self._collect_import_aliases(node)
         self.generic_visit(node)
-        self.scope_name_stack.pop()
+        self._pop_import_scope()
         return node
 
     def visit_FunctionDef(self, node):
-        self.scope_name_stack.append(node.name)
-        self.explicit_global_stack.append(set())
+        self._push_import_scope()
+        self._collect_import_aliases(node)
         self.generic_visit(node)
-        self.explicit_global_stack.pop()
-        self.scope_name_stack.pop()
+        self._pop_import_scope()
         return node
 
     def visit_AsyncFunctionDef(self, node):
-        self.scope_name_stack.append(node.name)
-        self.explicit_global_stack.append(set())
+        self._push_import_scope()
+        self._collect_import_aliases(node)
         self.generic_visit(node)
-        self.explicit_global_stack.pop()
-        self.scope_name_stack.pop()
+        self._pop_import_scope()
         return node
 
-    def _in_module_scope(self) -> bool:
-        return not self.explicit_global_stack
-
-    def _is_explicit_global_name(self, name: str) -> bool:
-        return bool(self.explicit_global_stack) and name in self.explicit_global_stack[-1]
+    def _collect_import_aliases(self, node):
+        alias_map = self._current_import_aliases()
+        for stmt in getattr(node, 'body', []):
+            if isinstance(stmt, ast.Import):
+                for name in stmt.names:
+                    if name.asname is None and '.' in name.name:
+                        continue
+                    local = name.asname or name.name
+                    alias_map[local] = {
+                        'asname': next(self.randomizer.random_name_gen),
+                    }
+            elif isinstance(stmt, ast.ImportFrom):
+                for name in stmt.names:
+                    if name.name == '*':
+                        continue
+                    local = name.asname or name.name
+                    imported_node = SYMBOL_MAP.find_import(stmt.module, name.name)
+                    if imported_node and hasattr(imported_node, 'greyed_out'): # indicates that is was actually obfusacted
+                        alias_map[local] = {
+                            'name': imported_node.obf_value['name'],
+                            'asname': next(self.randomizer.random_name_gen),
+                        }
+                    else:
+                        alias_map[local] = {
+                            'name': name.name,
+                            'asname': next(self.randomizer.random_name_gen),
+                        }
+            elif isinstance(stmt, (ast.ClassDef, ast.FunctionDef, ast.AsyncFunctionDef)):
+                # Stop collecting imports in nested scopes
+                continue
+            else:
+                self._collect_import_aliases(stmt)
 
     def visit_Import(self, node):
+        alias_map = self._current_import_aliases()
         for name in node.names:
-            original_bound_name = name.asname or name.name.split('.')[0]
-            local_name = self._lookup_import(original_bound_name)
-
-            if local_name is None:
-                if name.asname is None and '.' in name.name:
-                    self.binding_map[original_bound_name] = self._qualified_name(self._resolve_import_module(node), name.name)
-                    continue
-                # Use the full module name as the key for consistent aliasing
-                module_key = self._qualified_name(self._resolve_import_module(node), name.name)
-                local_name = self._local_import_name(name.name.split('.')[-1], module_key=module_key)
-                self._register_import(self._scope_key(), original_bound_name, local_name)
-                self.binding_map[local_name] = module_key
-
-            if name.asname is None and '.' in name.name:
-                continue
-
-            name.asname = local_name
+            local = name.asname or name.name
+            if local in alias_map:
+                name.asname = alias_map[local]['asname']
         return node
 
     def visit_ImportFrom(self, node):
-        resolved_module = self._resolve_import_module(node)
+        alias_map = self._current_import_aliases()
         for name in node.names:
-            if name.name == '*':
-                continue
-
-            original_bound_name = name.asname or name.name
-            qualified_name = f'{resolved_module}.{name.name}' if resolved_module else name.name
-            export_name = self._export_name(qualified_name)
-            imported_symbol_name = export_name if export_name is not None else name.name
-
-            local_name = self._lookup_import(original_bound_name)
-            if local_name is None:
-                # Use the qualified name as the key for consistent aliasing
-                local_name = self._local_import_name(imported_symbol_name, module_key=qualified_name)
-                self._register_import(self._scope_key(), original_bound_name, local_name)
-                self.binding_map[local_name] = qualified_name
-
-            name.name = imported_symbol_name
-            name.asname = local_name
-        return node
-
-    def visit_Global(self, node):
-        new_names = []
-        current_globals = self.explicit_global_stack[-1] if self.explicit_global_stack else None
-        for name in node.names:
-            if current_globals is not None:
-                current_globals.add(name)
-
-            mapped = self._lookup_import(name)
-            if mapped is not None:
-                new_names.append(mapped)
-                if current_globals is not None:
-                    current_globals.add(mapped)
-            else:
-                new_names.append(name)
-        node.names = new_names
-        return node
-
-    def visit_Nonlocal(self, node):
-        new_names = []
-        for name in node.names:
-            mapped = self._lookup_import(name)
-            if mapped is not None:
-                new_names.append(mapped)
-            else:
-                new_names.append(name)
-        node.names = new_names
-        return node
-
-    def visit_Attribute(self, node):
-        parts = []
-        current = node
-        while isinstance(current, ast.Attribute):
-            parts.append(current.attr)
-            current = current.value
-
-        if isinstance(current, ast.Name) and current.id in self.binding_map:
-            module_path = self.binding_map[current.id]
-            attr_parts = list(reversed(parts))
-            module_parts = module_path.split('.') if module_path else []
-
-            overlap = 0
-            max_overlap = min(len(module_parts), len(attr_parts))
-            for size in range(max_overlap, 0, -1):
-                if module_parts[-size:] == attr_parts[:size]:
-                    overlap = size
-                    break
-
-            candidate_parts = module_parts + attr_parts[overlap:]
-            if candidate_parts:
-                export_name = self._export_name('.'.join(candidate_parts))
-                if export_name is not None:
-                    node.attr = export_name
-
-        self.generic_visit(node)
+            local = name.asname or name.name
+            if local in alias_map:
+                name.name   = alias_map[local]['name']
+                name.asname = alias_map[local]['asname']
         return node
 
     def visit_Name(self, node):
-        mapped_name = self._lookup_import(node.id)
-        if mapped_name is None:
-            return node
-
-        if isinstance(node.ctx, ast.Load):
-            return ast.Name(id=mapped_name, ctx=node.ctx)
-
-        if isinstance(node.ctx, ast.Store) and (self._in_module_scope() or self._is_explicit_global_name(node.id)):
-            return ast.Name(id=mapped_name, ctx=node.ctx)
-
+        for import_stack in reversed(self._import_alias_stack):
+            if not node.id in import_stack:
+                continue
+            node.id = import_stack[node.id]['asname']
+            break
         return node
 
-    def visit_Delete(self, node):
-        for idx, target in enumerate(node.targets):
-            if isinstance(target, ast.Name):
-                mapped_name = self._lookup_import(target.id)
-                if mapped_name is not None:
-                    node.targets[idx] = ast.Name(id=mapped_name, ctx=ast.Del())
-        self.generic_visit(node)
-        return node
+    # def visit_Attribute(self, node):
+    #     # Obfuscate attribute chains for imported modules (like reference impl)
+    #     parts = []
+    #     current = node
+    #     while isinstance(current, ast.Attribute):
+    #         parts.append(current.attr)
+    #         current = current.value
+    #     if isinstance(current, ast.Name):
+    #         for import_stack in reversed(self._import_alias_stack):
+    #             if current.id in import_stack:
+    #                 # Build full path: import asname + . + attr chain
+    #                 obf_base = import_stack[current.id]['asname']
+    #                 # Optionally, could also obfuscate attribute chain if needed
+    #                 # For now, just replace base name
+    #                 current.id = obf_base
+    #                 break
+    #     self.generic_visit(node)
+    #     return node
