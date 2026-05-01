4c792125df0f368820d19ea8bc255fa30b8fd32c
Silas Kraume <silas.kraume1552@gmail.com>
2026-05-01 15:30:20 +0200

    3.14 compatibility

--- obfuspy/util/unparser.py
+++ obfuspy/util/unparser.py
@@ -2,8 +2,19 @@
 import ast
 
 def unparse(ast_obj, indentation_string=' ' * 4):
-    class Builder(ast._Unparser):
-        def fill(self, text=''):
-            self.maybe_newline()
-            self.write(indentation_string * self._indent + text)
+    try:
+        unparser = ast._Unparser
+    except (AttributeError, NameError):
+        from _ast_unparse import Unparser as _Unparser
+        unparser = _Unparser
+    class Builder(unparser):
+        def fill(self, text='', *, allow_semicolon=True):
+            if hasattr(self, '_in_interactive') and hasattr(self, '_indent') and (
+                self._in_interactive and not self._indent and allow_semicolon
+            ):
+                self.maybe_semicolon()
+                self.write(text)
+            else:
+                self.maybe_newline()
+                self.write(indentation_string * self._indent + text)
     return Builder().visit(ast_obj)
